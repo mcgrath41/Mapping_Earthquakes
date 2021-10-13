1e17b4618ff46a81a1713302a22f3887c7ba3368
Ryan McGrath <mcgrath41@gmail.com>
2021-10-13 18:59:46 -0500

    updated data.

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -18,38 +18,21 @@ let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/sate
 // Create a base layer that holds both maps.
 let baseMaps = {
   "Streets": streets,
-  "Satellite Streets": satelliteStreets
+  "Satellite": satelliteStreets
 };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-	center: [43.7, -79.3],
-	zoom: 11,
-	layers: [satelliteStreets]
+	center: [39.5, -98.5],
+	zoom: 3,
+	layers: [streets]
 });
 
 // Pass our map layers into our layer control and add the layer control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// Accessing the Toronto neighborhoods GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/mcgrath41/Mapping_Earthquakes/master/torontoNeighborhoods.json";
-
-// Create a style for the lines.
-let myStyle = {
-	fillColor: 'yellow	',
-	color: "blue",
-	weight: 1
-}
-
-// Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
-	console.log(data);
-	// Creating a GeoJSON layer with the retrieved data.
-	L.geoJSON(data,{
-		style: myStyle,
-		onEachFeature: function(feature, layer) {
-			layer.bindPopup("<h2> Neighborhood: " + feature.properties.AREA_NAME + "</h2>");
-		}
-	})
-	.addTo(map);
-	});
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJson(data).addTo(map);
+});
